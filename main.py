a885c6dbc86e9d23a4d7deb7ffed88199d12aa99
Paul Pedro <ppedro@avknowles.com>
2023-08-22 17:27:23 -0400

    password authentication works

--- main.py
+++ main.py
@@ -4,19 +4,13 @@ from datetime import date, timedelta
 import sqlite3
 import pandas as pd
 
+# shift setup
 shifts = ["Off", "Day Shift", "Night Shift"]
 schedules = [shift for shift in shifts for _ in range(2)]
+db_file = "users.db"
 
-# user authetication
 
-
-st.header("Siobhan's Shedule Check")
-
-col1, col2 = st.columns([3, 1])
-
-dt = col1.date_input("Choose a date: ", value=date.today(), min_value=date.today())
-
-def show_schedule():
+def show_schedule(dt):
     base_date = date(2023, 7, 17) # date when last off
     start = dt - timedelta(days=2)
     output = "| Date | Day | Schedule |\n| ------ | -------- |\n"
@@ -31,9 +25,41 @@ def show_schedule():
 
     return  output
 
-col2.header(" ")
-show = col2.button("Get schedule", on_click=show_schedule)
 
-if show:
-    st.write(show_schedule())
+def show_stuff():
+    col1, col2 = st.columns([3, 1])
+    dt = col1.date_input("Choose a date: ", value=date.today(), min_value=date.today())
+    col2.header(" ")
+    show = col2.button("Get schedule", on_click=show_schedule)
+    if show:
+        st.write(show_schedule(dt))
+
+def create_credentials():
+    # user authetication
+    db = sqlite3.connect(db_file)
+    qry = "SELECT rowid, * FROM users"
+    result = pd.read_sql_query(qry, db)
+
+    u = {}
+    for i in range(len(result)):
+        u[result.loc[i]["email"]] = {
+            "name": result.loc[i]["name"],
+            "password": result.loc[i]["hashedpwd"],
+        }
+
+    return {"usernames": u}
+
+authenticator = stauth.Authenticate(create_credentials(),
+    cookie_name='current_user', key='some_signature_key')
+
+name, authentication_status, email = authenticator.login('Login', 'sidebar')
+
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    st.header(f"{name}'s Shedule Check")
+    show_stuff()
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
 
